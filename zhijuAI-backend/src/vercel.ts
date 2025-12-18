07fdd2a98040c222be6d70438f8c038849a11e46
jinzi123-lad <850534432@qq.com>
2025-12-18 17:09:55 +0800

    0

--- zhijuAI-backend/src/vercel.ts
+++ zhijuAI-backend/src/vercel.ts
@@ -2,7 +2,7 @@ import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
-import * as express from 'express';
+import express from 'express';
 
 let app: any;
 
