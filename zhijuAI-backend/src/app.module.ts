6766156ee700d994755ed0c8e58a0e46891c9f46
jinzi123-lad <850534432@qq.com>
2025-12-18 17:22:19 +0800

    0

--- zhijuAI-backend/src/app.module.ts
+++ zhijuAI-backend/src/app.module.ts
@@ -1,3 +1,8 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { DashboardModule } from './dashboard/dashboard.module';
 import { SupabaseModule } from './common/supabase/supabase.module';
 
 @Module({
